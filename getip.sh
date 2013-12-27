229a78cb7d939697b17d87fc9460ec70cb2cc4c7
Anne Marie Merritt <a.m.merritt@gmail.com>
2013-12-26 20:04:10 -0800

    Updated systems this was tested on.

--- getip.sh
+++ getip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# Get the IP address from which the internet sees requests from this machine emanating.
+# Get the IP address from which the internet sees requests from this machine emanating.  Tested
+# on Mac OX 10.6.8, 10.9.1.
 
 curl -s http://checkip.dyndns.org | awk '{print $6}' | awk ' BEGIN { FS = "<" } { print $1 } '
